392f71436881cff54a1236e62664e099e4538413
Sebzzz69 <callenholmljungsebastian@gmail.com>
2022-04-06 16:32:03 +0200

    04.06.22
    The essentials for Snake is complete!!1!

--- Snake/Assets/Scripts/Snake.cs
+++ Snake/Assets/Scripts/Snake.cs
@@ -1,42 +1,113 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Snake : MonoBehaviour
 {
     private Vector2 direction = Vector2.right;
+    private List<Transform> _segments = new List<Transform>();
 
+    public Transform segmentPrefab;
+    public int initialSize = 3;
+    public Text score;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        ResetState();
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        if (direction.x != 0)
         {
-            direction = Vector2.up;
-        } else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            direction = Vector2.down;
-        } else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            direction = Vector2.right;
-        } else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            direction = Vector2.left;
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                direction = Vector2.up;
+            }
+            else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                direction = Vector2.down;
+            }
         }
+       
+        if (direction.y != 0)
+        {
+            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                direction = Vector2.right;
+            }
+            else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                direction = Vector2.left;
+            }
+        }
+        
     }
 
     private void FixedUpdate() 
     {
+        for (int i = _segments.Count - 1; i > 0; i--)
+        {
+            _segments[i].position = _segments[i - 1].position;
+        }
+
+
+        // Makes Snake move the way it's facing
+        // Rounding the values to make a grid
         this.transform.position = new Vector3(
             Mathf.Round(this.transform.position.x) + direction.x,
             Mathf.Round(this.transform.position.y) + direction.y,
             0.0f
             );
+    }
+
+    private void Grow()
+    {
+        Transform segment = Instantiate(this.segmentPrefab);
+        segment.position = _segments[_segments.Count - 1].position;
+
+        _segments.Add(segment);
+    }
+
+    private void ResetState()
+    {
+        
+
+        // Start at 1 to skip destroying the head
+        for (int i = 1; i < _segments.Count; i++)
+        {
+            Destroy(_segments[i].gameObject);
+            
+        }
+
+        // Clear list but add back the head
+        _segments.Clear();
+        _segments.Add(this.transform);
+        
+        // Makes the Snake start at a determened size
+        for (int i = 1; i < initialSize; i++)
+        {
+            _segments.Add(Instantiate(this.segmentPrefab));
+        }
+
+
+        this.transform.position = Vector3.zero;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Food"))
+        {
+            Grow();
+        } else if (other.gameObject.CompareTag("Obstacle"))
+        {
+            ResetState();
+            
+        }
+
     }
 }
